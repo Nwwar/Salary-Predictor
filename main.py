448d1b8255e8c0cebd251b955d02742db3d024cb
Ahmmed Nwwar <Ahmed.AMMN@fci.suezuni.edu.eg>
2023-07-08 18:37:28 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('Ask A Manager Salary Survey 2021 (Responses) - Form Responses 1.csv')
+
+df
+#%%
