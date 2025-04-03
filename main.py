909264a05bca8b9f9b519ee61bc7ca80710925e8
Nwwar <95590353+Nwwar@users.noreply.github.com>
2025-04-03 21:16:49 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-df = pd.read_csv('Ask A Manager Salary Survey 2021 (Responses) - Form Responses 1.csv')
-
-df
-#%%
